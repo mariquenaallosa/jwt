27fe58308ae30c9a85c75c2c1f188fac4120b8aa
Mariquena Allosa <allosamariquena@gmail.com>
2024-02-04 15:35:16 -0300

    AuthController

--- src/main/java/com/security/jwt/Controller/AuthController.java
+++ src/main/java/com/security/jwt/Controller/AuthController.java
@@ -1,2 +1,38 @@
-package com.security.jwt.Controller;public class AuthController {
+package com.security.jwt.Controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+
+
+@RestController
+@RequestMapping(value = "/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+
+
+    @PostMapping(value = "login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginDto login){
+        try{
+            return ResponseEntity.ok(authService.login(login));
+        }catch(Exception e){
+            return new ResponseEntity(e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping(value = "register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterDto register){
+        try{
+            return ResponseEntity.ok(authService.register(register));
+        }catch (Exception e){
+            return new ResponseEntity(e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
+    }
 }
